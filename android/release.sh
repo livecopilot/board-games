c9724ba8f4fdb2f38cd50c6d5c1d4e2e4928ed4d
changwj <yoursfengzhilian@gmail.com>
2025-07-02 12:28:27 +0800

    优化游戏状态显示逻辑，调整背景和边框样式，提升用户界面的一致性和视觉反馈；简化代码结构，增强可维护性。

---
+++ android/release.sh
@@ -0,0 +1,10 @@
+# @Author: changwj yoursfengzhilian@gmail.com
+# @Date: 2025-07-02 12:27:12
+# @LastEditors: changwj yoursfengzhilian@gmail.com
+# @LastEditTime: 2025-07-02 12:27:17
+# @FilePath: /board-games/android/release.sh
+# @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+###
+
+
+./gradlew assembleRelease
